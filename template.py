036dbb64c7a4a85e1b525cfdfe52a6ec28bf2ead
SkellyBG <skellybg245@gmail.com>
2022-12-06 16:22:25 +1100

    Add template file

---
+++ template.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+f = open("input.txt", "r")
+
+def nums(line) -> list:
+    return [int(x) for x in line if x.strip()]
+
+def part1():
+    for line in f:
+        pass
+
+
+def part2():
+    for line in f:
+        pass
+
+part1()
+part2()
