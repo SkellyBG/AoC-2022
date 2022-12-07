acfaf7c7966f400190539bba634c77f0bbc66c40
SkellyBG <skellybg245@gmail.com>
2022-12-07 17:01:39 +1100

    Update template

--- template.py
+++ template.py
@@ -1,16 +1,18 @@
 #!/bin/python3
 
-f = open("input.txt", "r")
+
 
 def nums(line) -> list:
     return [int(x) for x in line if x.strip()]
 
 def part1():
+    f = open("input.txt", "r")
     for line in f:
         pass
 
 
 def part2():
+    f = open("input.txt", "r")
     for line in f:
         pass
 
