0cc7bf63feb087b14ae5a13935165f759a8befd3
SkellyBG <skellybg245@gmail.com>
2022-12-12 14:22:12 +1100

    Day 10

---
+++ days/10/solve.py
@@ -0,0 +1,67 @@
+#!/bin/python3
+import re
+
+dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+def nums(line) -> list:
+    return [int(x) for x in line if x.strip()]
+
+def part1():
+    f = open("input.txt", "r")
+    count = 0
+    registers = [1]
+    pc = 1
+    for line in f:
+        line = line.split()
+        if line[0] == "noop":
+            if ((pc - 20) % 40) == 0:
+                count += pc * registers[0]
+            pc += 1
+        elif line[0] == "addx":
+            if ((pc - 20) % 40) == 0:
+                count += pc * registers[0]
+            pc += 1
+            if ((pc - 20) % 40) == 0:
+                count += pc * registers[0]
+            pc += 1
+            registers[0] += int(line[1])
+    print(count)
+
+def part2():
+    f = open("input.txt", "r")
+    count = 0
+    registers = [1]
+    pc = 0
+    for line in f:
+        line = line.split()
+        if line[0] == "noop":
+            if pc in range(registers[0] - 1, registers[0] + 2):
+                print("#", end = "")
+            else:
+                print(".", end = "")
+            pc += 1
+            if pc % 40 == 0:
+                pc = 0
+                print()
+        elif line[0] == "addx":
+            if pc in range(registers[0] - 1, registers[0] + 2):
+                print("#", end = "")
+            else:
+                print(".", end = "")
+            pc += 1
+            if pc % 40 == 0:
+                pc = 0
+                print()
+            if pc in range(registers[0] - 1, registers[0] + 2):
+                print("#", end = "")
+            else:
+                print(".", end = "")
+            pc += 1
+            if pc % 40 == 0:
+                pc = 0
+                print()
+            registers[0] += int(line[1])
+    print(count)
+
+part1()
+part2()
