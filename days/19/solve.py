27ae1d29f225a94858ba484773d09681d6d1c715
SkellyBG <skellybg245@gmail.com>
2022-12-22 20:34:56 +1100

    Day 19 template

---
+++ days/19/solve.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+import re
+import functools
+from collections import deque
+
+dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+def nums(line) -> list:
+    return [int(x) for x in line if x.strip()]
+
+def part1() -> None:
+    f = open("input.txt", "r")
+    for line in f:
+        line = line.rstrip("\n")
+
+def part2() -> None:
+    f = open("input.txt", "r")
+    for line in f:
+        line = line.rstrip("\n")
+
+part1()
+part2()
